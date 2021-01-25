8b27a36f70cf5b3c24424110073e3e58a545147e
smcommits <sm.anas@outlook.com>
2021-01-25 20:48:58 +0530

    Fix Rubocop Errors]

--- app/services/request_destroyer.rb
+++ app/services/request_destroyer.rb
@@ -1,7 +1,7 @@
 class RequestDestroyer < ApplicationService
   attr_reader :request
 
-  def initialize(friend_id: nil, user_id: nil) 
+  def initialize(friend_id: nil, user_id: nil)
     @request = FriendRequest.find_by(user_id: friend_id, friend_id: user_id)
   end
 
